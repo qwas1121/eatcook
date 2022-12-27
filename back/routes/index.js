e6d31cf50a6c467797efaa396dfb101e3aa2cbce
qwas1121 <qwas1121@naver.com>
2022-12-27 18:19:24 +0900

    Update index.js

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var router = express.Router();
-var path = require("path");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
